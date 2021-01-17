2c92ac20a4e65d08d77695dece34b10f14cf7b17
BuildTools <admin@ydb.ovh>
2021-01-17 21:47:30 +0100

    UpdateCmd

--- dist/commands/developer/update.js
+++ dist/commands/developer/update.js
@@ -21,43 +21,47 @@ module.exports = {
                 .setDescription('<a:Loading:800458223891120199> Preparing for Update.')
                 .setColor('#edc01c');
             let embmsg = await message.channel.send(embed);
-            child_process_1.exec('git pull git@chika:Chika-Discord/Chika.git', (err, stdout, stderr) => {
-                if (!err) {
-                    setTimeout(() => {
-                        embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<a:Loading:800458223891120199>   Installing Node Modules...\n<:idle:507524745378398210>   Restarting Bot...');
+            embed.setDescription('<a:Loading:800458223891120199>   Pulling changes from GitHub...\n<:idle:507524745378398210>   Installing Node Modules...\n<:idle:507524745378398210>   Restarting Bot...');
+            embmsg.edit(embed);
+            setTimeout(() => {
+                child_process_1.exec('git pull git@chika:Chika-Discord/Chika.git', (err, stdout, stderr) => {
+                    if (!err) {
+                        setTimeout(() => {
+                            embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<a:Loading:800458223891120199>   Installing Node Modules...\n<:idle:507524745378398210>   Restarting Bot...');
+                            embmsg.edit(embed);
+                            child_process_1.exec('npm i', (err, stdout, stderr) => {
+                                if (!err) {
+                                    embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:greenTick:718980916449378365>   Installing Node Modules...\n<a:Loading:800458223891120199>   Restarting Bot...');
+                                    embmsg.edit(embed);
+                                    setTimeout(() => {
+                                        embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:greenTick:718980916449378365>   Installing Node Modules...\n<:redTick:718980916076347423>   Restarting Bot...\n\nAn unknown Error occured while Updating.\nStopping the Update.')
+                                            .setColor('#ed311c');
+                                        embed.edit(embed);
+                                        child_process_1.exec('pm2 restart chika', (err, stdout, stderr) => {
+                                            if (err) {
+                                                embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:greenTick:718980916449378365>   Installing Node Modules...\n<:greenTick:718980916449378365>   Restarting Bot...\n\nSuccessfully updated the Bot!')
+                                                    .setColor('#1ced3b');
+                                                embmsg.edit(embed);
+                                            }
+                                            else {
+                                            }
+                                        });
+                                    }, 2000);
+                                }
+                                else {
+                                    embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:redTick:718980916076347423>   Installing Node Modules...\n<:idle:507524745378398210>   Restarting Bot...\n\nAn unknown Error occured while Updating.\nStopping the Update.');
+                                    embed.edit(embed);
+                                }
+                            });
+                        }, 2000);
+                    }
+                    else {
+                        embed.setDescription('<:redTick:718980916076347423>   Pulling changes from GitHub...\n<:idle:507524745378398210>   Installing Node Modules...\n<:idle:507524745378398210>   Restarting Bot...')
+                            .setColor('#ed311c');
                         embmsg.edit(embed);
-                        child_process_1.exec('npm i', (err, stdout, stderr) => {
-                            if (!err) {
-                                embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:greenTick:718980916449378365>   Installing Node Modules...\n<a:Loading:800458223891120199>   Restarting Bot...');
-                                embmsg.edit(embed);
-                                setTimeout(() => {
-                                    child_process_1.exec('pm2 restart chika', (err, stdout, stderr) => {
-                                        if (err) {
-                                            embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:greenTick:718980916449378365>   Installing Node Modules...\n<:greenTick:718980916449378365>   Restarting Bot...\n\nSuccessfully updated the Bot!')
-                                                .setColor('#1ced3b');
-                                            embmsg.edit(embed);
-                                        }
-                                        else {
-                                            embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:greenTick:718980916449378365>   Installing Node Modules...\n<:redTick:718980916076347423>   Restarting Bot...\n\nAn unknown Error occured while Updating.\nStopping the Update.')
-                                                .setColor('#ed311c');
-                                            embed.edit(embed);
-                                        }
-                                    });
-                                }, 2000);
-                            }
-                            else {
-                                embed.setDescription('<:greenTick:718980916449378365>   Pulling changes from GitHub...\n<:redTick:718980916076347423>   Installing Node Modules...\n<:idle:507524745378398210>   Restarting Bot...\n\nAn unknown Error occured while Updating.\nStopping the Update.');
-                                embed.edit(embed);
-                            }
-                        });
-                    }, 2000);
-                }
-                else {
-                    embed.setDescription('<:redTick:718980916076347423>   Pulling changes from GitHub...\n<:idle:507524745378398210>   Installing Node Modules...\n<:idle:507524745378398210>   Restarting Bot...')
-                        .setColor('#ed311c');
-                    embmsg.edit(embed);
-                }
-            });
+                    }
+                });
+            }, 2000);
         }
         else {
             client.embeds.error(message.channel, 'This Instance is not running on the Server.');
