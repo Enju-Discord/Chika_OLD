f6f05d50c2a46e8f94517266579b966112ae5e7c
Newtox <futzy261@gmail.com>
2021-01-30 01:12:51 +0100

    🚀

--- dist/commands/economy/yen.js
+++ dist/commands/economy/yen.js
@@ -3,23 +3,23 @@ module.exports = {
     description: 'cmd.yen.description',
     usage: 'cmd.yen.usage',
     args: true,
-    dm: true,
+    dm: false,
     group: 'Economy',
     cooldown: 10,
     bot_permissions: [],
     user_permissions: [],
     aliases: ['money', 'balance'],
     async execute(message, args, client, prefix) {
-        const target = (message.channel.type === 'dm') ? message.author : (message.mentions.members.first().user || message.guild.members.cache.get(args[0]).user || message.member.user);
+        const target = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         client.con.query('SELECT * FROM economy WHERE id = ?;', [target.id], async (error, result) => {
             if (error)
                 return client.embeds.error(message.channel, '```js\n' + error + '```');
             if (result.length === 1) {
-                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.tag));
+                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.user.tag));
             }
             else {
                 client.con.query('INSERT INTO economy(id, yen) VALUES(?, ?);', [target.id, 0]);
-                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.tag));
+                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.user.tag));
             }
         });
     }
--- src/commands/economy/yen.ts
+++ src/commands/economy/yen.ts
@@ -3,22 +3,22 @@ module.exports = {
     description: 'cmd.yen.description',
     usage: 'cmd.yen.usage',
     args: true,
-    dm: true,
+    dm: false,
     group: 'Economy',
     cooldown: 10,
     bot_permissions: [],
     user_permissions: [],
     aliases: ['money', 'balance'],
     async execute(message: any, args: any, client: any, prefix: any) {
-        const target: any = (message.channel.type === 'dm') ? message.author : (message.mentions.members.first().user || message.guild.members.cache.get(args[0]).user || message.member.user);
+        const target: any = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
         client.con.query('SELECT * FROM economy WHERE id = ?;', [target.id], async (error: any, result: any) => {
             if (error) return client.embeds.error(message.channel, '```js\n' + error + '```');
             if (result.length === 1) {
-                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.tag));
+                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.user.tag));
             } else {
                 client.con.query('INSERT INTO economy(id, yen) VALUES(?, ?);', [target.id, 0]);
-                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.tag));
+                return client.embeds.success(message.channel, (await client.strings(message.guild, 'cmd.yen.result')).replace('$yen', client.functions.numberWithCommas(result[0].yen)).replace('$user', target.user.tag));
             }
         });
     }
